9ffd80a92599e09be7ebe25e56532d8475df2cbd
Hoodluc <202321032@projecao.edu.br>
2024-11-29 21:29:37 -0300

    Revert "Merge branch 'main' of https://github.com/mfsjunior/ProjetoGuara"
    This reverts commit 051d9208accb24834a13399298b879a2467ab161, reversing
    changes made to a9bb6bca116df4119012e8a1c7d6881ac0b67ca8.

--- App/Views/layouts/menu.php
+++ App/Views/layouts/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -10,21 +10,13 @@
             <a class="navbar-brand" href="http://<?php echo APP_HOST; ?>">Projeto Guará</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav dropdown">
+            <ul class="nav navbar-nav">
                 <li <?php if($viewVar['nameController'] == "HomeController") { ?> class="active" <?php } ?>>
                     <a href="http://<?php echo APP_HOST; ?>" >Home</a>
                 </li>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Usuário
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item"  href="http://<?php echo APP_HOST; ?>/usuario/cadastro">>CADASTRAR</a><br>
-          <a class="dropdown-item"  href="http://<?php echo APP_HOST; ?>/usuario/listar">>LISTAR</a>
-        </div>
-        
-                    
-        
+                <li <?php if($viewVar['nameController'] == "UsuarioController") { ?> class="active" <?php } ?>>
+                    <a href="http://<?php echo APP_HOST; ?>/usuario/cadastro" >Cadastro de Usuário</a>
+                </li>
                 <li <?php if($viewVar['nameController'] == "ClienteController") { ?> class="active" <?php } ?>>
                     <a href="http://<?php echo APP_HOST; ?>/cliente/cadastro" >Cadastro de Cliente</a>
                 </li>
